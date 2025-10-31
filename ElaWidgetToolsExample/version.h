ff279c2a2b4a41b69c549e14f527957ef5af98cb
ZM <1529156314@qq.com>
2025-10-31 10:41:33 +0800

    新增version.h文件以定义版本信息

---
+++ ElaWidgetToolsExample/version.h
@@ -0,0 +1,21 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+#define VER_FILEVERSION             1,0,0,0
+#define VER_FILEVERSION_STR         "1.0.0.0\0"
+
+// 第一个数字：主版本号 (Major)
+// 第二个数字：次版本号 (Minor)
+// 第三个数字：修订号 (Patch)
+// 第四个数字：构建号 (Build)
+#define VER_PRODUCTVERSION          1,0,3,0
+#define VER_PRODUCTVERSION_STR      "1.0.3.0\0"
+
+#define VER_COMPANYNAME_STR         "ZMTools"
+#define VER_FILEDESCRIPTION_STR     "ZMTools Example Application"
+#define VER_INTERNALNAME_STR        "ZMToolsExample"
+#define VER_LEGALCOPYRIGHT_STR      "Copyright (C) 2025 ZMTools"
+#define VER_ORIGINALFILENAME_STR    "ZMTools.exe"
+#define VER_PRODUCTNAME_STR         "ZMTools Example"
+
+#endif // VERSION_H
